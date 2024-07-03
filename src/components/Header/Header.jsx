cef49d333ed44888101e8e44d90c320a8b88cb74
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2024-07-03 10:31:14 +0600

    navbar add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,9 @@ const Header = () => {
         <div>
             <div className="navbar bg-base-100">
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">
+                        <img src="/src/assets/images/"></img>
+                    </a>
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal px-1">
