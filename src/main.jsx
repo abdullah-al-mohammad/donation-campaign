e4c540b810d2b0090838b026ebd7d749cd4e40e6
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2024-07-06 10:14:50 +0600

    add json file

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/Root/Root';
+import Home from './components/Home/Home';
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      }
+    ]
   }
 ])
 
